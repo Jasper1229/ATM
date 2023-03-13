8dd66db31f50ee38b968d62eb45769577e4c66d3
Jasper <jasper.stephens1@gmail.com>
2023-03-12 19:54:16 -0500

    Account class made

---
+++ ATM/Account.cs
@@ -0,0 +1,52 @@
+﻿namespace ATM;
+
+public class Account
+{
+    private double balance;
+    private string name;
+    private string password;
+    private int account_id;
+
+    public Account(string name, string password, int account_id)
+    {
+        this.name = name;
+        this.password = password;
+        this.account_id = account_id;
+        this.balance = 0;
+    }
+
+    public void setBalance(double number)
+    {
+        this.balance = number;
+    }
+
+    public void modifyBalance(double number)
+    {
+        this.balance += number;
+    }
+
+    public void setName(string name)
+    {
+        this.name = name;
+    }
+
+    public void setPassword(string password)
+    {
+        this.password = password;
+    }
+
+    public int getAccountID()
+    {
+        return this.account_id;
+    }
+
+    public string getName()
+    {
+        return this.name;
+    }
+
+    public string getPassword()
+    {
+        return this.password;
+    }
+}
